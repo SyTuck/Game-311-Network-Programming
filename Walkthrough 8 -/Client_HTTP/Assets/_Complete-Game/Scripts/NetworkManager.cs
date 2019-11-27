354edd5a63b0d41f45805095cd588859e500936a
SyTuck <44281354+SyTuck@users.noreply.github.com>
2019-11-27 11:49:25 -0500

    completed walkthrough 8

---
+++ Walkthrough 8 -/Client_HTTP/Assets/_Complete-Game/Scripts/NetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.InteropServices;
+using System;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager _instance = null;
+    
+    public static NetworkManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<NetworkManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("NetworkManager");
+                    _instance = go.AddComponent<NetworkManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+}
